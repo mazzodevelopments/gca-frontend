363afb4a5716633f3e1fb1014b26a72268acff8d
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-04-12 19:55:19 -0300

    Search filter functionality

--- gca-frontend/src/components/ClientSearch.tsx
+++ gca-frontend/src/components/ClientSearch.tsx
@@ -8,9 +8,17 @@ import { mainH2 } from '../styleClassNames';
 
 export default function ClientSearch() {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState<{ client: Client }[] | null>(
-        clients
-    );
+    const [results, setResults] = useState<Client[]>(clients);
+
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const searchTerm = event.target.value.toLowerCase();
+        setTerm(searchTerm);
+        const filteredClients = clients.filter((client) => {
+            const fullName = `${client.name} ${client.lastName}`.toLowerCase();
+            return fullName.includes(searchTerm);
+        });
+        setResults(filteredClients);
+    };
 
     return (
         <div className="mx-auto flex flex-col">
@@ -22,7 +30,7 @@ export default function ClientSearch() {
                         type="text"
                         placeholder="Buscar clientes"
                         value={term}
-                        onChange={(e) => setTerm(e.target.value)}
+                        onChange={handleSearch} // Update search results on input change
                     />
                     <button className=" text-black px-4 py-2 hover:text-gray-500 focus:outline-none">
                         <GoSearch />
@@ -31,7 +39,11 @@ export default function ClientSearch() {
             </div>
             <div className="mt-4">
                 <h2 className={mainH2}>Resultados:</h2>
-                {results && <ClientList clients={results} />}
+                {results.length > 0 ? (
+                    <ClientList clients={results} />
+                ) : (
+                    <p>No se encontraron resultados.</p>
+                )}
             </div>
         </div>
     );
