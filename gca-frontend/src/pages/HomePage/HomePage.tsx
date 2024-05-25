4e02628a47393db6daeca7700c5b96bc3db5aa5d
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-05-25 00:54:53 -0300

    HomePage

--- gca-frontend/src/pages/HomePage/HomePage.tsx
+++ gca-frontend/src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
+import useAuth from '../../hooks/use-auth-context';
+import useData from '../../hooks/use-data';
+
+import { HomeLoaderResults } from './homeLoader';
+
 import FeaturedList from './components/FeaturedList';
 import ClientSearch from './components/ClientSearch';
-import useAuth from '../../hooks/use-auth-context';
 import Container from '../../components/Container';
 import ButtonHeader from '../../components/ButtonHeader';
 import Header from '../../components/Header';
-import useData from '../../hooks/use-data';
-import { HomeLoaderResults } from './homeLoader';
 
 export default function HomePage() {
     const { username } = useAuth();
