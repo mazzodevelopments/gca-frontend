ecbcd610ee3dba49e98c9396cf4809cf8f75c4dd
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-06-12 14:03:04 -0300

    clientService

--- gca-frontend/src/services/clientService.ts
+++ gca-frontend/src/services/clientService.ts
@@ -1,26 +1,63 @@
+import axios from 'axios';
+import adaptClientPageData, {
+    adaptClientData,
+    adaptClientListData
+} from '../adapters/clientAdapter';
 import { AddableClient, Client } from '../types/client';
 
-export const createClient = (client: AddableClient) => {
-    console.log(client);
-    return 1; // DEBE DEVOLVER EL ID DEL CLIENTE CREADO
-};
+const URL = 'http://192.168.1.23:3000';
 
-export const updateClient = (editedClient: Client) => {
-    console.log(editedClient);
-};
+export async function createClient(
+    client: AddableClient
+): Promise<number | null> {
+    try {
+        const response = await axios.post(
+            `${URL}/client`,
+            JSON.stringify(client),
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+        return response.data.clientId;
+    } catch (error) {
+        console.error('Error creating client:', error);
+        return null;
+    }
+}
 
-export const deleteClient = (clientId: number) => {
-    console.log(clientId + ' eliminado');
-};
+export async function updateClient(editedClient: Client): Promise<boolean> {
+    try {
+        await axios.put(`${URL}/client`, editedClient, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log('Cliente actualizado:', editedClient);
+        return true;
+    } catch (error) {
+        throw new Error(`Error al actualizar el cliente: ${error}`);
+    }
+}
+
+export async function deleteClient(clientId: number): Promise<boolean> {
+    try {
+        await axios.delete(`${URL}/client`, { data: { clientId } });
+        console.log(`Cliente con ID ${clientId} eliminado`);
+        return true;
+    } catch (error) {
+        console.error('Error al eliminar el cliente:', error);
+        return false;
+    }
+}
 
 export async function getClientData(clientId: number) {
     try {
         console.log('Datos ' + clientId);
-        const response = await fetch(
-            'https://6600a57387c91a1164198f19.mockapi.io/gca/clients'
-        );
-        const data = await response.json();
-        return data;
+        const response = await axios.get(`${URL}/clientdata/${clientId}`);
+        console.log(response);
+        return adaptClientData(response.data);
     } catch (e) {
         console.log(e);
         return null;
@@ -29,12 +66,8 @@ export async function getClientData(clientId: number) {
 
 export async function getClientPageData(clientId: number) {
     try {
-        console.log('Datos ' + clientId);
-        const response = await fetch(
-            'https://6600a57387c91a1164198f19.mockapi.io/gca/clients'
-        );
-        const data = await response.json();
-        return data;
+        const response = await axios.get(`${URL}/clientpage/${clientId}`);
+        return adaptClientPageData(response.data);
     } catch (e) {
         console.log(e);
         return null;
@@ -43,16 +76,11 @@ export async function getClientPageData(clientId: number) {
 
 export async function getAllClientsByUserId(userId: number) {
     try {
-        console.log('clientes de ' + userId);
-        const response = await fetch(
-            'https://6600a57387c91a1164198f19.mockapi.io/gca/clients'
-        );
-        const data = await response.json();
-        return data;
+        console.log('Clientes del user con id: ' + userId);
+        const response = await axios.get(`${URL}/userclients/${userId}`);
+        return adaptClientListData(response.data);
     } catch (e) {
         console.log(e);
         return [];
     }
 }
-
-// http://192.168.1.23:5001/clientspreview/12'
