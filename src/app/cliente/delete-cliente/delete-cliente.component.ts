753c303be05f2bcbae9f337ff2662a8f8ee9835f
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-07-21 13:20:04 -0300

    Delete cliente component & showcliente

--- src/app/cliente/delete-cliente/delete-cliente.component.ts
+++ src/app/cliente/delete-cliente/delete-cliente.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {ClienteService} from "../cliente.service";
+import {Cliente} from "../cliente";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-delete-cliente',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './delete-cliente.component.css'
 })
 export class DeleteClienteComponent {
+  @Input() cliente!: Cliente;
+  showModal = false;
 
+  constructor(private clienteService: ClienteService, private router: Router) {
+  }
+
+  deleteCliente() {
+    this.clienteService.deleteCliente(this.cliente.id)
+    this.showModal = false;
+    this.router.navigateByUrl('gestor');
+  }
 }
