b5e5b7aa012b897ae46b7383e28400785fc2730d
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-08-03 14:27:07 -0300

    Rm imports

--- src/app/cliente/create-cliente/create-cliente.component.ts
+++ src/app/cliente/create-cliente/create-cliente.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {Cliente} from "../cliente";
-import {Location} from '@angular/common';
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-cliente',
