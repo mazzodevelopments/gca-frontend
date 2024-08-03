4ce44754e8a9ace89150575add78a82c2e6c59ed
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-08-03 10:48:41 -0300

    Mostrar polizas en el showclient

--- src/app/cliente/show-cliente/show-cliente.component.ts
+++ src/app/cliente/show-cliente/show-cliente.component.ts
@@ -3,6 +3,8 @@ import {ActivatedRoute} from '@angular/router';
 import {ClienteService} from "../cliente.service";
 import {Cliente} from "../cliente";
 import {take} from "rxjs";
+import {Poliza} from "../../poliza/poliza";
+import {PolizaService} from "../../poliza/poliza.service";
 
 @Component({
   selector: 'app-show-cliente',
@@ -20,19 +22,27 @@ export class ShowClienteComponent implements OnInit {
     telefono: '',
     pais: ''
   };
+  polizas!: Poliza[];
 
-  constructor(private route: ActivatedRoute, private clienteService: ClienteService) {
+  constructor(
+    private route: ActivatedRoute,
+    private clienteService: ClienteService,
+    private polizaService: PolizaService
+  ) {
   }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      this.clienteId = params.get('id');
+      this.clienteId = params.get('clienteId');
       if (this.clienteId) {
+        // CARGAR DATOS DEL CLIENTE
         this.clienteService.getCliente(this.clienteId)
           .pipe(take(1))
           .subscribe(cliente => {
             this.cliente = cliente[0];
           });
+        // CARGAR POLIZAS DEL CLIENTE
+        this.polizas = this.polizaService.getAllPolizas()
       } else {
         console.error('Cliente ID no encontrado en la URL');
       }
