3eb487b1f9b201940df00edebcd0493c234b029d
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-08-03 12:31:31 -0300

    poliza module

--- src/app/poliza/poliza.module.ts
+++ src/app/poliza/poliza.module.ts
@@ -1,10 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ShowPolizaComponent} from './show-poliza/show-poliza.component';
+import {RouterLink} from "@angular/router";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ShowPolizaComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RouterLink
   ]
 })
 export class PolizaModule {
