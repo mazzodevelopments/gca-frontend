e1e76ab11e0c476b55c1736ac8a0daea9b494e30
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-08-22 22:14:16 -0300

    Date & Validations

--- src/app/cliente/form-cliente/form-cliente.component.ts
+++ src/app/cliente/form-cliente/form-cliente.component.ts
@@ -18,13 +18,17 @@ export class FormClienteComponent implements OnInit {
   ngOnInit(): void {
     const {nombre, apellido, direccion, fechaNacimiento, telefono, pais} = this.cliente;
     this.clienteForm = this.fb.group({
-      nombre: new FormControl(nombre, Validators.required),
-      apellido: new FormControl(apellido, Validators.required),
-      direccion: new FormControl(direccion, Validators.required),
-      fechaNacimiento: new FormControl(fechaNacimiento, Validators.required),
-      telefono: new FormControl(telefono, Validators.required),
-      pais: new FormControl(pais, Validators.required)
+      nombre: new FormControl(nombre, [Validators.required]),
+      apellido: new FormControl(apellido, [Validators.required]),
+      direccion: new FormControl(direccion, [Validators.required]),
+      fechaNacimiento: new FormControl(this.formatDate(fechaNacimiento), [Validators.required, this.validarFecha]),
+      telefono: new FormControl(telefono, [Validators.required, Validators.pattern(/^[0-9]{10}$/)]),
+      pais: new FormControl(pais, [Validators.required])
     });
+  }
+
+  formatDate(date: Date): string {
+    return date ? new Date(date).toISOString().substring(0, 10) : '' // YYYY-MM-DD
   }
 
   onSubmit() {
@@ -32,5 +36,14 @@ export class FormClienteComponent implements OnInit {
       return;
     }
     this.submitCliente.emit(this.clienteForm.value);
+  }
+
+  validarFecha(control: FormControl): { [key: string]: boolean } | null {
+    const fecha = new Date(control.value);
+    const fechaActual = new Date();
+    if (fecha >= fechaActual) {
+      return {fechaInvalida: true};
+    }
+    return null;
   }
 }
